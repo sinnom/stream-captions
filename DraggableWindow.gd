d1efe7778c81274aae32ae04cfacc395f63a8f2c
sinnom <106713618+sinnom@users.noreply.github.com>
2023-03-12 14:13:25 -0500

    fix: stop display window flashing and jumping when dragged

--- DraggableWindow.gd
+++ DraggableWindow.gd
@@ -7,20 +7,21 @@ var window_id: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  if window:
-    window.window_input.connect(on_window_input)
+	if window:
+		window.window_input.connect(on_window_input)
 
 
 func on_window_input(event: InputEvent):
-  if event is InputEventMouseButton:
-    if event.button_index == 1:
-      is_dragging = !is_dragging
-      drag_start_position = get_local_mouse_position()
-      window_id = event.get_window_id()
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			is_dragging = !is_dragging
+			drag_start_position = get_local_mouse_position()
+			window_id = event.get_window_id()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-  if is_dragging:
-    var new_position = DisplayServer.window_get_position() + (get_global_mouse_position() as Vector2i) - drag_start_position
-    DisplayServer.window_set_position(new_position, window_id)
-  pass
+	if is_dragging:
+		var new_position = DisplayServer.window_get_position(window_id) + (get_global_mouse_position() as Vector2i) - drag_start_position
+		DisplayServer.window_set_position(new_position, window_id)
+		pass
+	
